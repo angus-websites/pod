9bbbf8dff30ab5b9c449ffe2dd1e34479a0ec2d4
Angus <angus.goody@gmail.com>
2022-10-17 00:14:36 +0100

    Added component

---
+++ resources/js/Components/Buttons/PrimaryButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <Button class="inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-lime-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-lime-700">
+        <slot/>
+    </Button>
+</template>
+
+<script>
+export default {
+    name: "PrimaryButton",
+}
+</script>
+
---
+++ resources/js/Components/Buttons/SecondaryLinkButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <a class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+        <slot/>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "SecondaryLinkButton",
+}
+</script>
+
