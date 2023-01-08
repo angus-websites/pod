fbe5ecd1cef5f4355fd737d0dc657e5bbf758278
Angus <angus.goody@gmail.com>
2023-01-08 20:46:30 +0000

    Basic tailwind tiptap

--- resources/js/Components/entry/EntryEdit.vue
+++ resources/js/Components/entry/EntryEdit.vue
@@ -1,17 +1,17 @@
 
 <template>
-    <Tiny />
+    <Tiptap :content="content" />
 </template>
 
 
 <script>
-//import Tiptap from '@/Components/Tiptap.vue';
+import Tiptap from '@/Components/Tiptap.vue';
 //import Editor from '@/Components/Editor.vue';
-import Tiny from '@/Components/Tiny.vue';
+//import Tiny from '@/Components/Tiny.vue';
 
 export default {
     components: {
-        Tiny
+        Tiptap
     },
     props: {
         content: String
