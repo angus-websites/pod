e2a76ec39f4114cbcdc63678b0240f423ede6f44
Angus <angus.goody@gmail.com>
2023-02-02 23:48:01 +0000

    Tweak to header

---
+++ resources/js/Components/entry/EntryHeaderCreate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="lg:flex lg:items-center lg:justify-between">
+    <div class="min-w-0 flex-1">
+      <EntryBreadcrumbs title="New Entry" />
+      <Heading1 text="New Entry" />
+      <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+        <div class="mt-2 flex items-center text-sm text-gray-500">
+            Enter the details for this new entry
+        </div>
+      </div>
+    </div>
+    <div class="mt-5 flex lg:mt-0 lg:ml-4">
+    </div>
+  </div>
+</template>
+
+<script setup>
+import EntryBreadcrumbs from "@/Components/entry/EntryBreadcrumbs.vue";
+import Heading1 from "@/Components/headings/Heading1.vue";
+
+</script>
--- resources/js/Pages/Entry/Create.vue
+++ resources/js/Pages/Entry/Create.vue
@@ -3,7 +3,7 @@
   <AppLayout title="New Entry">
     <PageContainer>
         <div class="flex flex-col space-y-5">
-            <Heading1 text="New Entry" />
+            <EntryHeaderCreate />
             <EntryEdit :entry="new_entry" :create="true" />
         </div>
     </PageContainer>
@@ -15,8 +15,8 @@
 //imports
 import AppLayout from "@/Layouts/AppLayout.vue";
 import PageContainer from "@/Components/_util/PageContainer.vue";
-import Heading1 from "@/Components/headings/Heading1.vue";
 import EntryEdit from "@/Components/entry/EntryEdit.vue";
+import EntryHeaderCreate from "@/Components/entry/EntryHeaderCreate.vue";
 
 const props = defineProps(['new_entry'])
 
