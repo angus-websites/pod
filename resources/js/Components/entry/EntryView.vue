00d8b61a34c3c8e7d8e9644db5be379873bf8b5a
Angus <angus.goody@gmail.com>
2023-02-13 19:26:33 +0000

    Basic working load and view

--- resources/js/Components/entry/EntryView.vue
+++ resources/js/Components/entry/EntryView.vue
@@ -2,21 +2,11 @@
 <template>
 
     <div class="flex flex-col space-y-5">
-
-      <div v-for="item in entry.data">
-        {{ item }}
+      <div v-for="item in entry.template.fields">
+        <p v-if="entry.data[item.id]"><u>{{item.label}}:</u> {{entry.data[item.id]}}</p>
+        <br>
       </div>
     </div>
-
-<!--     <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-      <div class="mt-2 flex items-center text-sm text-gray-500">
-        <CalendarIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-        {{ entry.date }}
-      </div>
-    </div>
-
-    <article class="prose prose-zinc" v-html="entry.content">
-    </article> -->
 </template>
 
 
