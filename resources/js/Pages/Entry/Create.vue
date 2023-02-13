4f3c7070324f00e9c3348445df97bdeccbf418c4
Angus <angus.goody@gmail.com>
2023-02-13 21:07:34 +0000

    New entry screen loading

--- resources/js/Pages/Entry/Create.vue
+++ resources/js/Pages/Entry/Create.vue
@@ -2,7 +2,7 @@
 <template>
   <AppLayout title="New Entry">
     <PageContainer>
-        <div class="flex flex-col space-y-5">
+        <div class="flex flex-col space-y-10">
             <EntryHeaderCreate />
             
 
@@ -12,25 +12,10 @@
               <select v-model="selected_template" class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                 <option v-for="template in templates" :value="template">{{template.name}}</option>
               </select>
+              <p class="mt-2 text-sm text-gray-500" id="email-description">Select a template for this new entry</p>
             </div>
 
-            <!-- Render -->
-            <p>Current template: {{selected_template.name}} </p>
-
-            <form class="flex flex-col space-y-5">
-              <div v-for="field in selected_template.fields">
-                <label :for="field.id">{{field.label}}</label>
-                <input v-if="field.type == 'text'" type="text" :name="field.id" :id="field.id">
-                <input v-if="field.type == 'date'" type="date" :name="field.id" :id="field.id">
-                <textarea v-else-if="field.type == 'textarea'" :name="field.id" :id="field.id"></textarea>
-              </div>
-
-              <!-- Save button -->
-              <div class="mt-5">
-                  <PrimaryButton type="submit">Save</PrimaryButton>
-              </div>
-
-            </form>
+            <EntryEdit :entry="entry" :create="true" />
 
 
         </div>
@@ -42,6 +27,8 @@
 
 //imports
 import { ref, onMounted } from 'vue'
+import { reactive, computed } from 'vue'
+
 import AppLayout from "@/Layouts/AppLayout.vue";
 import PageContainer from "@/Components/_util/PageContainer.vue";
 import EntryEdit from "@/Components/entry/EntryEdit.vue";
@@ -53,6 +40,25 @@ const props = defineProps(['new_entry', 'templates'])
 // Templates
 const selected_template = ref(props.templates[0]);
 
+// Modify a new entry object based on the selected template
+const entry = computed(() => {
+
+  let current_template = selected_template.value
+  props.new_entry.template = current_template
+
+  let data = [];
+  console.log(selected_template.value)
+
+  // Populate the data with nulls
+  for (var i = 0; i < current_template.fields.length; i++) {
+    data[current_template.fields[i].id] = null;
+  }
+
+  // Attatch data
+  props.new_entry['data'] = data;
+
+  return props.new_entry
+})
 
 
 </script>
