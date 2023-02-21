4dd9177fb30e28985202c70f06b450d4c8904a69
Angus <angus.goody@gmail.com>
2023-02-21 22:44:40 +0000

    Added search bar to index

--- resources/js/Pages/Entry/Index.vue
+++ resources/js/Pages/Entry/Index.vue
@@ -13,6 +13,17 @@
                         <span>New Entry</span>
                     </PrimaryButton>
                 </div>
+
+                <!-- Search section -->
+                            <div class="w-full max-w-lg lg:max-w-xs my-5">
+                              <label for="search" class="sr-only">Search</label>
+                              <div class="relative text-gray-400 focus-within:text-gray-500">
+                                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                  <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
+                                </div>
+                                <input id="search" class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-gray-500 focus:border-purple-500 focus:placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-purple-500 sm:text-sm" placeholder="Search" type="search" name="search" />
+                              </div>
+                            </div>
                 <!-- Entry table-->
                 <EntryTable :entries="entries" class="my-8 border" />
             </div>
@@ -45,6 +56,7 @@ import PageContainer from "@/Components/_util/PageContainer.vue";
 import EntryTable from "@/Components/dashboard/EntryTable.vue";
 import PrimaryButton from "@/Components/buttons/PrimaryButton.vue";
 import Heading1 from "@/Components/headings/Heading1.vue";
+import {MagnifyingGlassIcon} from '@heroicons/vue/20/solid';
 
 export default {
     components: {
@@ -53,6 +65,7 @@ export default {
         AppLayout,
         Heading1,
         PrimaryButton,
+        MagnifyingGlassIcon,
     },
     props: ["entries"],
     mounted() {
