b7602f3d02cecd5b2a958c842bc2d1926c11f81b
Angus <angus.goody@gmail.com>
2023-02-25 14:24:27 +0000

    Added search functionality to controller

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -28,9 +28,14 @@ class EntryController extends Controller
      * Display a listing of the resource.
      *
      */
-    public function index()
+    public function index(Request $request)
     {
-        $entries =  EntryResource::collection(Auth::user()->entries()->paginate(15));
+
+        $entries =  EntryResource::collection(
+            Auth::user()->entries()->when($request->input('search'), function($query, $search){
+                return $query->where('data.title', 'like', '%'.$search.'%');
+            })->paginate(15)
+        );
         return Inertia::render('Entry/Index', ['entries' => $entries]);
     }
 
--- resources/js/Pages/Entry/Index.vue
+++ resources/js/Pages/Entry/Index.vue
@@ -13,17 +13,16 @@
                         <span>New Entry</span>
                     </PrimaryButton>
                 </div>
-
                 <!-- Search section -->
-                            <div class="w-full max-w-lg lg:max-w-xs my-5">
-                              <label for="search" class="sr-only">Search</label>
-                              <div class="relative text-gray-400 focus-within:text-gray-500">
-                                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                  <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
-                                </div>
-                                <input id="search" class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-gray-500 focus:border-purple-500 focus:placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-purple-500 sm:text-sm" placeholder="Search" type="search" name="search" />
-                              </div>
-                            </div>
+                <div class="w-full max-w-lg lg:max-w-xs my-5">
+                  <label for="search" class="sr-only">Search</label>
+                  <div class="relative text-gray-400 focus-within:text-gray-500">
+                    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                      <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
+                    </div>
+                    <input v-model="searchQuery" id="search" class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-gray-500 focus:border-purple-500 focus:placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-purple-500 sm:text-sm" placeholder="Search" type="search" name="search" />
+                  </div>
+                </div>
                 <!-- Entry table-->
                 <EntryTable :entries="entries" class="my-8 border" />
             </div>
@@ -67,10 +66,22 @@ export default {
         PrimaryButton,
         MagnifyingGlassIcon,
     },
+    data() {
+      return {
+        searchQuery: 'Am I truly an alligator?'  
+      };
+    },
     props: ["entries"],
     mounted() {
         console.log(this.entries["data"])
-    }
+    },
+    watch: {
+      searchQuery(query, old){
+        this.$inertia.get('/entries', pickBy(query), { preserveState: true })
+        console.log(query)
+      }
+    },
+
 }
 </script>
 
