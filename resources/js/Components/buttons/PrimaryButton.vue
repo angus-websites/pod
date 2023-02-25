97cc0932158f3d9edfb7faa42c870dd1bb9c916b
Angus <angus.goody@gmail.com>
2023-02-25 15:39:59 +0000

    Tweaked navbar

--- resources/js/Components/buttons/PrimaryButton.vue
+++ resources/js/Components/buttons/PrimaryButton.vue
@@ -31,7 +31,7 @@ export default {
         },
 
         baseClasses(){
-            return "inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-primary text-base font-medium text-white shadow-sm enabled:hover:bg-primary-600 disabled:opacity-50";
+            return "inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-primary text-base font-medium text-white shadow-sm hover:bg-primary-600 disabled:opacity-50 disabled:hover:bg-primary";
         }
 
     }
