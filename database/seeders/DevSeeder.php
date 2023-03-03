0c6d0bdf3ac5e560a79a2734c4200b49b92b83b3
Angus <angus.goody@gmail.com>
2023-03-03 19:35:31 +0000

    Can randomly assign groups to users

--- database/seeders/DevSeeder.php
+++ database/seeders/DevSeeder.php
@@ -10,6 +10,9 @@ use App\Models\User;
 use App\Models\Entry;
 use App\Models\Template;
 
+use JustSteveKing\Laravel\FeatureFlags\Models\FeatureGroup;
+
+
 class DevSeeder extends Seeder
 {
     /**
@@ -21,6 +24,12 @@ class DevSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Template::truncate();
+
+        DB::table('feature_groups')->truncate();
+        DB::table('feature_feature_group')->truncate();
+        DB::table('feature_group_user')->truncate();
+        DB::table('feature_user')->truncate();
+        
         User::truncate();
         Entry::truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
@@ -31,9 +40,23 @@ class DevSeeder extends Seeder
         // Admin seed
         $this->call(AdminSeeder::class);
 
+        $groupA = FeatureGroup::create([
+            'name' => 'Group A'
+        ]);
+
+        $groupB = FeatureGroup::create([
+            'name' => 'Group B'
+        ]);
+
+
+        $featureGroups = [$groupA, $groupB];
+
+
         // Create some users with entries
-        User::factory()->count(5)
-            ->hasEntries(12)
-            ->create();
+        User::factory()->count(5)->hasEntries(12)->create()->each(function ($u) use(&$featureGroups) {
+            $random_group = $featureGroups[array_rand($featureGroups, 1)];
+            $u->addToGroup($random_group->name);
+        });;
+
     }
 }
