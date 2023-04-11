281157d305fb279956cd3b6e078a56f35f465499
Angus <angus.goody@gmail.com>
2023-04-11 18:40:51 +0100

    Added more tests

--- tests/Feature/CVTest.php
+++ tests/Feature/CVTest.php
@@ -8,8 +8,6 @@ use Database\Seeders\FeatureSeeder;
 use Database\Seeders\RoleSeeder;
 use Database\Seeders\TemplateSeeder;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use JustSteveKing\Laravel\FeatureFlags\Models\Feature;
 use Tests\TestCase;
@@ -17,6 +15,15 @@ use Tests\TestCase;
 class CVTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Seed features and ensure active
+        $this->seed(FeatureSeeder::class);
+        Feature::where("name", "=", "CV Builder")->update(['active'=>'1']);
+    }
 
     /**
      * Test a normal user can
@@ -26,9 +33,6 @@ class CVTest extends TestCase
      */
     public function test_user_with_feature_can_access_cv_builder_when_active()
     {
-        $this->seed(FeatureSeeder::class);
-        // Ensure we have the features & it's enabled
-        Feature::where("name", "=", "CV Builder")->update(['active'=>'1']);
 
         // Create user
         $user = User::factory()->create();
@@ -56,10 +60,6 @@ class CVTest extends TestCase
 
         $this->seed(RoleSeeder::class);
         $this->seed(TemplateSeeder::class);
-        $this->seed(FeatureSeeder::class);
-
-        // Ensure we have the features & it's enabled
-        Feature::where("name", "=", "CV Builder")->update(['active'=>'1']);
 
         // Create the admin
         $admin = User::create([
@@ -71,7 +71,6 @@ class CVTest extends TestCase
         $admin->role_id = Role::where('name', '=', 'Super Admin')->firstOrFail()->id;
         $admin->save();
 
-
         // Acting as this user
         $this->actingAs($admin);
 
@@ -80,4 +79,66 @@ class CVTest extends TestCase
         $response->assertStatus(200);
 
     }
+
+    public function test_user_without_feature_cannot_access_cv_builder()
+    {
+        // Create user
+        $user = User::factory()->create();
+
+        // Acting as this user
+        $this->actingAs($user);
+
+        // Test we cannot access the page
+        $response = $this->get(route('cv'));
+        $response->assertStatus(403);
+    }
+
+    public function test_user_with_feature_cannot_access_cv_builder_when_inactive()
+    {
+        // Disable the feature
+        Feature::where("name", "=", "CV Builder")->update(['active'=>'0']);
+
+        // Create user
+        $user = User::factory()->create();
+
+        // Give the CV builder feature
+        $user->giveFeature("CV Builder");
+
+        // Acting as this user
+        $this->actingAs($user);
+
+        // Test we cannot access the page
+        $response = $this->get(route('cv'));
+        $response->assertStatus(403);
+    }
+
+    public function test_admin_cannot_access_cv_builder_if_it_doesnt_exist()
+    {
+        // Delete the row
+        Feature::where("name", "=", "CV Builder")->delete();
+
+        // -------- Create admin --------
+
+        $this->seed(RoleSeeder::class);
+        $this->seed(TemplateSeeder::class);
+
+        // Create the admin
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $admin->role_id = Role::where('name', '=', 'Super Admin')->firstOrFail()->id;
+        $admin->save();
+
+        // Acting as this user
+        $this->actingAs($admin);
+
+        // Test we can access the page
+        $response = $this->get(route('cv'));
+        $response->assertStatus(403);
+    }
+
+
 }
