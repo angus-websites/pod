b9b05a781ef437605e87342f02b00404193c9229
Angus <angus.goody@gmail.com>
2023-04-12 17:29:39 +0100

    Fixed registration

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -78,7 +78,7 @@ const submit = () => {
                 <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
                     <div class="relative flex items-start">
                         <div class="flex h-6 items-center">
-                            <input id="comments" aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+                            <input v-model="form.terms" id="terms" aria-describedby="comments-description" name="terms" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
                         </div>
                         <div class="ml-3 text-sm leading-6">
                             <label for="comments" class="font-medium text-gray-900">
