aee4732e23ee6fb44ad7459d5e88594e075ebe8e
Angus <angus.goody@gmail.com>
2023-04-14 15:18:23 +0100

    today's date as default

--- resources/js/Components/template/TemplateDate.vue
+++ resources/js/Components/template/TemplateDate.vue
@@ -1,6 +1,6 @@
 <template>
-    <input 
-    :value="modelValue"
+    <input
+    :value="newValue"
     @input="$emit('update:modelValue', $event.target.value)"
     type="date" :name="field.id" :id="field.id" :class="baseClasses" :required="field.required">
 </template>
@@ -13,6 +13,11 @@ export default {
     computed: {
         baseClasses(){
             return "block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm";
+        },
+        newValue()
+        {
+            
+            return this.modelValue ? this.modelValue : new Date().toISOString().slice(0,10);
         }
 
     }
