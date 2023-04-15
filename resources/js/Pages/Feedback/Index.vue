86b9297d3bd3203d80486055257a5aca9f2d831c
Angus <angus.goody@gmail.com>
2023-04-15 19:42:51 +0100

    Added empty state

--- resources/js/Pages/Feedback/Index.vue
+++ resources/js/Pages/Feedback/Index.vue
@@ -13,57 +13,62 @@
                     <!-- Main container -->
                     <div class="mt-10 flex flex-col space-y-10">
 
-                        <!-- Question group -->
-                        <div v-for="(group, g) in feedbackGroups.data">
+                        <template v-if="feedbackGroups.data > 0">
+                            <!-- Question group -->
+                            <div v-for="(group, g) in feedbackGroups.data">
 
-                            <div class="relative mb-10">
-                                <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                                    <div class="w-full border-t border-gray-300" />
+                                <div class="relative mb-10">
+                                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                        <div class="w-full border-t border-gray-300" />
+                                    </div>
+                                    <div class="relative flex justify-center">
+                                        <span class="bg-white px-3 text-base font-semibold leading-6 text-gray-900">Section {{g+1}}</span>
+                                    </div>
                                 </div>
-                                <div class="relative flex justify-center">
-                                    <span class="bg-white px-3 text-base font-semibold leading-6 text-gray-900">Section {{g+1}}</span>
-                                </div>
-                            </div>
 
-                            <div class="bg-white rounded-lg border p-5 pt-1">
+                                <div class="bg-white rounded-lg border p-5 pt-1">
 
-                                <Heading2 class="m-0">{{ group.name }}</Heading2>
-                                <small>{{ group.caption }}</small>
+                                    <Heading2 class="m-0">{{ group.name }}</Heading2>
+                                    <small>{{ group.caption }}</small>
 
-                                <!-- Questions -->
-                                <div class="mt-5 flex flex-col space-y-5 md:max-w-lg">
-                                    <div v-for="(question, q) in group.questions">
-                                        <Heading3><b>{{g+1}}.{{q+1}}</b> &nbsp; {{question.name}}</Heading3>
+                                    <!-- Questions -->
+                                    <div class="mt-5 flex flex-col space-y-5 md:max-w-lg">
+                                        <div v-for="(question, q) in group.questions">
+                                            <Heading3><b>{{g+1}}.{{q+1}}</b> &nbsp; {{question.name}}</Heading3>
 
-                                        <template v-if="question.type == 'text'">
-                                            <div class="mt-2">
-                                                <textarea v-model="feedback.answers[question.id]"   rows="4" name="comment" id="comment" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                            </div>
-                                        </template>
+                                            <template v-if="question.type == 'text'">
+                                                <div class="mt-2">
+                                                    <textarea v-model="feedback.answers[question.id]"   rows="4" name="comment" id="comment" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                </div>
+                                            </template>
 
-                                        <template v-else-if="question.type == 'radio'">
-                                            <fieldset>
-                                                <div class="mt-4 divide-y divide-gray-200 border-b border-t border-gray-200">
-                                                    <div v-for="(option, index) in question.data.options" :key="index" class="relative flex items-start py-4">
-                                                        <div class="min-w-0 flex-1 text-sm leading-6">
-                                                            <label :for="`side-${option.id}`" class="select-none font-medium text-gray-900">{{ option.label }}</label>
-                                                        </div>
-                                                        <div class="ml-3 flex h-6 items-center">
-                                                            <input v-model="feedback.answers[question.id]" :value="option.id" :id="`side-${option.id}`" :name="question.name" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+                                            <template v-else-if="question.type == 'radio'">
+                                                <fieldset>
+                                                    <div class="mt-4 divide-y divide-gray-200 border-b border-t border-gray-200">
+                                                        <div v-for="(option, index) in question.data.options" :key="index" class="relative flex items-start py-4">
+                                                            <div class="min-w-0 flex-1 text-sm leading-6">
+                                                                <label :for="`side-${option.id}`" class="select-none font-medium text-gray-900">{{ option.label }}</label>
+                                                            </div>
+                                                            <div class="ml-3 flex h-6 items-center">
+                                                                <input v-model="feedback.answers[question.id]" :value="option.id" :id="`side-${option.id}`" :name="question.name" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </fieldset>
-                                        </template>
+                                                </fieldset>
+                                            </template>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <!-- Submit -->
-                         <div class="mt-10">
-                             <PrimaryButton :disabled="form.processing">Submit</PrimaryButton>
-                         </div>
+                            <!-- Submit -->
+                             <div class="mt-10">
+                                 <PrimaryButton :disabled="form.processing">Submit</PrimaryButton>
+                             </div>
+                        </template>
+                        <div v-else>
+                            <p>No feedback questions available, please check back later</p>
+                        </div>
                     </div>
                 </form>
             </div>
