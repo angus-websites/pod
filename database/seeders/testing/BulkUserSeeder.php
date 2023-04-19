8788bbe9b882f8f4cf5a7e87ea7ea57c334c6713
Angus <angus.goody@gmail.com>
2023-04-19 15:58:35 +0100

    Added bulk user seeder

---
+++ database/seeders/testing/BulkUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders\testing;
+
+use App\Models\Entry;
+use App\Models\User;
+use Database\Factories\EntryFactory;
+use Illuminate\Database\Seeder;
+use JustSteveKing\Laravel\FeatureFlags\Models\FeatureGroup;
+
+class BulkUserSeeder extends Seeder
+{
+    public function run(){
+
+        User::factory()->count(5)->create()->each(function ($u){
+
+            // Assign a random group to this user
+            $random_group_name = FeatureGroup::all()->where("active", "1")->random()->name;
+            $u->addToGroup($random_group_name);
+
+            $n = rand(5,50);
+
+            Entry::factory()->count($n)->create(['user_id' => $u->id]);
+        });
+    }
+}
