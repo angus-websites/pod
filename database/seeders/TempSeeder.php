a8033726c15e5de4e0812182fc706e569b821a9d
Angus <angus.goody@gmail.com>
2023-04-25 19:35:38 +0100

    Added user seeder

--- database/seeders/TempSeeder.php
+++ database/seeders/TempSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Entry;
+use App\Models\User;
 use Database\Seeders\core\FeedbackSeeder;
 use Illuminate\Database\Seeder;
+use JustSteveKing\Laravel\FeatureFlags\Models\FeatureGroup;
 
 class TempSeeder extends Seeder
 {
@@ -15,7 +18,17 @@ class TempSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(FeedbackSeeder::class);
+        User::factory()->count(10)->create()->each(function ($u){
+
+            // Assign a random group to this user
+            $random_group_name = FeatureGroup::all()->where("active", "1")->random()->name;
+            $u->addToGroup($random_group_name);
+
+            // Random number of entries
+            $n = rand(5,20);
+
+            Entry::factory()->count($n)->create(['user_id' => $u->id]);
+        });
 
     }
 }
