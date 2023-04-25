55ac12804caef56fba3c0decf71dcf50db658ca7
Angus <angus.goody@gmail.com>
2023-04-25 19:29:19 +0100

    Added TempSeeder

---
+++ database/seeders/TempSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Database\Seeders\core\FeedbackSeeder;
+use Illuminate\Database\Seeder;
+
+class TempSeeder extends Seeder
+{
+    /**
+     * Use this seeder to seed
+     * database from a production environment
+     */
+    public function run()
+    {
+        $this->call(FeedbackSeeder::class);
+
+    }
+}
